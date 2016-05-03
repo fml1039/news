1a6124960dea8968da3e3781daf1410928f182d2
fml1039 <jp2011212847@qmul.ac.uk>
2016-05-03 19:37:47 +0100

    replace send_keys with set value of javascript variable function

--- test.py
+++ test.py
@@ -4,18 +4,22 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 from selenium.webdriver.support.ui import Select
 
-mykey = "日本地震"
+mykey = u"日本 地震"
 print mykey
+
 browser = webdriver.Firefox()  
 browser.get('http://search.gmw.cn/search.do?advType=news')  
+'''
+The following statement can be used to fill the keyword form, yet I don't want to use that
+
 input =  browser.find_element_by_css_selector('input[type="text"]')
-
-input.send_keys(u"日本地震")
+input.send_keys(mykey)
 print "done"
+'''
 
-#date = browser.find_element_by_id("time").value = "8"
+browser.execute_script("document.getElementById('keyword').value='"+mykey+"'")
+
 select = Select(browser.find_element_by_id("time"))
-#select.deselect_all()
 select.select_by_visible_text("2011")
 button =  browser.find_element_by_css_selector('button')
 button.click()
